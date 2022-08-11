1c5a5b1e8e39ac111ee371e1134bf1c727471ceb
lijianru <2219269374@qq.com>
2022-08-11 19:25:45 +0800

    支持sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, normalizePath } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '${variablePath}';`,
+      },
+    },
+  },
+});

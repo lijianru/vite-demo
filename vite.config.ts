baa8665f0902a9dc7a20c5bb892128ef6915df0e
lijianru <2219269374@qq.com>
2022-08-12 09:39:30 +0800

    postcss & autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, normalizePath } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import autoprefixer from 'autoprefixer';
 
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
@@ -15,6 +16,13 @@ export default defineConfig({
     },
     modules: {
       generateScopedName: '[name]_[local]_[hash:base64:5]',
+    },
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+        }),
+      ],
     },
   },
 });

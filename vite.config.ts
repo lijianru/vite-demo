c5bd9400e2c27bd7050fc9b1efe0e6326ce40f20
lijianru <2219269374@qq.com>
2022-08-12 19:22:35 +0800

    stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig, normalizePath } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import viteEslint from 'vite-plugin-eslint';
+import viteStylelint from 'vite-plugin-stylelint';
 
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteEslint()],
+  plugins: [
+    react(),
+    viteEslint(),
+    viteStylelint({
+      exclude: ['windicss', 'node_modules']
+    })
+  ],
   css: {
     preprocessorOptions: {
       scss: {

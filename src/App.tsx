a2a48efb1a6c3a5412e47028ed7f5e9a47fc7bf3
lijianru <2219269374@qq.com>
2022-08-12 12:04:54 +0800

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import './App.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Header />
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img
+          className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+          src={reactLogo}
+          alt="Woman's Face"
+        />
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">Erin Lindford</p>
+            <p className="text-slate-500 font-medium">Product Engineer</p>
+          </div>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Message
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       <Footer />
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, normalizePath } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import autoprefixer from 'autoprefixer';
 
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
@@ -16,13 +15,6 @@ export default defineConfig({
     },
     modules: {
       generateScopedName: '[name]_[local]_[hash:base64:5]',
-    },
-    postcss: {
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
-        }),
-      ],
     },
   },
 });
